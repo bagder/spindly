15e6a883801fc710bf822a15a80e665cb671054a
Thomas (nezza-_-) Roth <code@stacksmashing.net>
2011-04-21 18:29:13 +0200

    Extended spdy_frame unittests.

--- tests/check_spdy_frame.c
+++ tests/check_spdy_frame.c
@@ -1,5 +1,6 @@
 #include "check_spdy_frame.h"
 #include "../src/spdy_frame.h"
+#include "../src/spdy_control_frame.h"
 
 #include "testdata.h"
 
@@ -8,6 +9,8 @@ START_TEST (test_spdy_frame_parse_header)
 	spdy_frame frame;
 	spdy_frame_parse_header(&frame, test_frame_header);
 	fail_unless(frame.type == SPDY_CONTROL_FRAME, "Frame type detection failed.");
+	spdy_control_frame *ctrl = (spdy_control_frame*)frame.frame;
+	fail_unless(ctrl->version == 2, "Creation of control frame failed.");
 }
 END_TEST
 
