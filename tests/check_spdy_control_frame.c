f8fd549e38d662df11cd5c40796dc6a84975dfca
Thomas (nezza-_-) Roth <code@stacksmashing.net>
2011-04-21 17:38:06 +0200

    Added unittest for spdy_control_frame_pack_header

--- tests/check_spdy_control_frame.c
+++ tests/check_spdy_control_frame.c
@@ -14,13 +14,30 @@ START_TEST (test_spdy_control_frame_parse_header)
 }
 END_TEST
 
+START_TEST (test_spdy_control_frame_pack_header)
+{
+	spdy_control_frame frame = {
+		.version = 2,
+		.type = 1,
+		.flags = 1,
+		.length = 288
+	};
+	char *out=NULL;
+	int ret = spdy_control_frame_pack_header(&out, &frame);
+	fail_unless(ret == 0, "spdy_control_frame_pack_header failed.");
+	fail_unless(memcmp(out, test_frame_header, 8) == 0, "Packed data is invalid.");
+}
+END_TEST
+
 Suite * spdy_control_frame_suite()
 {
 	Suite *s = suite_create("spdy_control_frame");
 	TCase *tc_core = tcase_create("spdy_control_frame_parse_header");
 	tcase_add_test(tc_core, test_spdy_control_frame_parse_header);
 	suite_add_tcase (s, tc_core);
-
+	tc_core = tcase_create("spdy_control_frame_pack_header");
+	tcase_add_test(tc_core, test_spdy_control_frame_pack_header);
+	suite_add_tcase(s, tc_core);
 	return s;
 }
 
