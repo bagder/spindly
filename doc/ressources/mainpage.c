dad1b4ca688b1700e735aa2a909250e9fbc186ff
Thomas (nezza-_-) Roth <code@stacksmashing.net>
2011-04-22 02:36:47 +0200

    Added doc/ressources for additional documentation ressources.

---
+++ doc/ressources/mainpage.c
@@ -0,0 +1,11 @@
+/*! \mainpage libspdy
+ * \section intro_sec Introduction
+ *
+ * Libspdy is a C implementation of Googles SPDY protocol. It's main goals are:
+ *  - Completely implementing the SPDY protocol stanard.
+ *  - Being lightweight: As few dependencies as possible.
+ *  - High testcoverage: Every function and branch should be tested.
+ *   - Unittests using Check
+ *   - Coverage check using LCOV
+ *  - Good documentation: The API documentation itself and the code should be as documented as possible - without cluttering the source.
+ */
