ee9f59b817d2fc982969d3fe0e1cf1ea75590291
Thomas (nezza-_-) Roth <code@stacksmashing.net>
2011-04-22 20:16:56 +0200

    spdy_control_frame: Added SPDY_CTRL_TYPES enum containing all control frame types.

--- src/spdy_control_frame.h
+++ src/spdy_control_frame.h
@@ -4,6 +4,22 @@
 #include <stdint.h>
 
 /**
+ * SPDY control frame types.
+ * List of all SPDY control frame types.
+ */
+enum SPDY_CTRL_TYPES {
+	SPDY_CTRL_SYN_STREAM=1,   /*!< SYN_STREAM */
+	SPDY_CTRL_SYN_REPLY=2,    /*!< SYN_REPLY */
+	SPDY_CTRL_RST_STREAM=3,   /*!< RST_STREAM */
+	SPDY_CTRL_SETTINGS=4,     /*!< SETTINGS */
+	SPDY_CTRL_NOOP=5,         /*!< NOOP */
+	SPDY_CTRL_PING=6,         /*!< PING */
+	SPDY_CTRL_GOAWAY=7,       /*!< GOAWAY */
+	SPDY_CTRL_HEADERS=8,      /*!< HEADERS */
+	SPDY_CTRL_WINDOW_UPDATE=9 /*!< WINDOW_UPDATE */
+};
+
+/**
  * Control frame
  * Contains all data (including the data payload) of a data frame.
  */
@@ -12,7 +28,7 @@ typedef struct {
 	uint16_t type;      /*!< 16 bit type */
 	uint8_t flags;      /*!< 8 bit flags */
 	uint32_t length;    /*!< 24 bit length */
-	char *data;/*!< Frame payload */
+	char *data;         /*!< Frame payload */
 } spdy_control_frame;
 
 int spdy_control_frame_parse_header(spdy_control_frame *frame, char *data);
