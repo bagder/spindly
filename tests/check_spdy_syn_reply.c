27cef56cb54ff4ed1dd3ef003efb71a9d2cdb13e
Thomas (nezza-_-) Roth <code@stacksmashing.net>
2011-04-24 03:24:08 +0200

    Added spdy_syn_reply tests.

---
+++ tests/check_spdy_syn_reply.c
@@ -0,0 +1,40 @@
+#include "check_spdy_syn_reply.h"
+#include "../src/spdy_syn_reply.h"
+
+#include "testdata.h"
+
+START_TEST (test_spdy_syn_reply_parse_header)
+{
+	int ret;
+	spdy_syn_reply syn_reply;
+	ret = spdy_syn_reply_parse_header(&syn_reply, test_control_syn_reply_frame+8, 55);
+	fail_unless(ret == 0, "spdy_syn_reply_parse failed.");
+	fail_unless(syn_reply.stream_id == 1, "Stream ID parsing failed.");
+}
+END_TEST
+#include <stdio.h>
+START_TEST (test_spdy_syn_reply_parse)
+{
+	int ret;
+	spdy_zlib_context zlib_ctx;
+	spdy_syn_reply syn_reply;
+	ret = spdy_zlib_inflate_init(&zlib_ctx);
+	fail_unless(ret == 0, "spdy_zlib_inflate_init failed.");
+	ret = spdy_syn_reply_parse(&syn_reply, test_control_syn_reply_frame+8, 55, &zlib_ctx);
+	fail_unless(ret == 0, "spdy_syn_reply_parse failed.");
+}
+END_TEST
+
+Suite * spdy_syn_reply_suite()
+{
+	Suite *s = suite_create("spdy_syn_reply");
+	TCase *tc_core = tcase_create("spdy_syn_reply_parse_header");
+	tcase_add_test(tc_core, test_spdy_syn_reply_parse_header);
+	suite_add_tcase (s, tc_core);
+	tc_core = tcase_create("spdy_syn_reply_parse");
+	tcase_add_test(tc_core, test_spdy_syn_reply_parse);
+	suite_add_tcase (s, tc_core);
+
+	return s;
+}
+
---
+++ tests/check_spdy_syn_reply.h
@@ -0,0 +1,9 @@
+#ifndef CHECK_SPDY_SYN_REPLY_H_
+#define CHECK_SPDY_SYN_REPLY_H_ 1
+
+#include <check.h>
+
+Suite * spdy_syn_reply_suite();
+
+#endif
+
