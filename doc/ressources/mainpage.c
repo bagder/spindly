daaf1dc02281c4b26e7816683f95bae4518ce7ce
Thomas (nezza-_-) Roth <code@stacksmashing.net>
2011-05-02 16:08:58 +0200

    mainpage: changes.

--- doc/ressources/mainpage.c
+++ doc/ressources/mainpage.c
@@ -1,13 +1,20 @@
 /*! \mainpage libspdy
  * \section intro_sec Introduction
+ * Libspdy is in early work in progress, but if you like living on the
+ * bleeding edge you can check out the git repo:
+ *  - git clone http://libspdy.org/git/ libspdy
  *
- * Libspdy is a C implementation of Googles SPDY protocol. Its main goals are:
+ * Libspdy is a C (Okay, C99!) implementation of Googles SPDY protocol. Its main goals are:
  *  - Completely implementing the SPDY protocol stanard.
- *  - Being lightweight: As few dependencies as possible.
+ *  - Being lightweight: As few dependencies as possible. (Right now: zlib (and gzip coming up), as well as Check for the unit tests.)
  *  - High testcoverage: Every function is tested.
  *   - Unittests using Check
  *   - Coverage check using LCOV
+ *  - Compiler independent (C99 support is needed.):
+ *   - Tested with gcc and clang
  *  - Good documentation: The API documentation itself and the code should be as documented as possible - without cluttering the source.
  *
- *  Author: Thomas Roth
+ * If you want to learn more about SPDY, check out the official site by Google: http://www.chromium.org/spdy/
+ *
+ *  Author: Thomas Roth <code@stacksmashing.net>
  */
