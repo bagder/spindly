981ed92912c3c68c78a3a8f48dc6323325f2dacd
Thomas (nezza-_-) Roth <code@stacksmashing.net>
2011-06-07 12:01:28 +0200

    Checks for spdy_data_frame_parse

--- tests/testdata.h
+++ tests/testdata.h
@@ -5,6 +5,7 @@ extern char *test_control_syn_stream_frame;
 extern char *test_control_syn_reply_frame;
 extern char *test_control_rst_stream_frame;
 extern char *test_data_frame_header;
+extern char *test_data_frame;
 extern char *test_nv_block;
 
 #endif
