1c051be0c5b7d0cac498b96f24264974b9e682df
Thomas (nezza-_-) Roth <code@stacksmashing.net>
2011-06-16 18:26:15 +0200

    read_stream: Fixed for working with control frames.

--- bin/read_stream.c
+++ bin/read_stream.c
@@ -17,11 +17,7 @@ int main(int argc, char *argv[]) {
 		return EXIT_FAILURE;
 	}
 
-	spdy_zlib_context zlib_ctx;
-	if(spdy_zlib_inflate_init(&zlib_ctx) < 0) {
-		printf("Failed to infalte init");
-		return EXIT_FAILURE;
-	}
+
 	int ret = SPDY_ERROR_INSUFFICIENT_DATA;
 	char pkg[2048];
 	int ptr=0;
@@ -33,26 +29,54 @@ int main(int argc, char *argv[]) {
 		printf("Can't read/\n");
 		return EXIT_FAILURE;
 	}
+	ptr=8;
 	spdy_data_use(&data, pkg, 8);
+	spdy_zlib_context zlib_ctx;
+	if(spdy_zlib_inflate_init(&zlib_ctx) < 0) {
+		printf("Failed to infalte init");
+		return EXIT_FAILURE;
+	}
 	ret = spdy_frame_parse(
 			&frm,
 			&data,
 			&zlib_ctx);
-	while(ret == SPDY_ERROR_INSUFFICIENT_DATA) {
-		if(fread(pkg+ptr, 1, 8, f) != 8) {
-			printf("Can't read.\n");
+	while(1) {
+		if(spdy_zlib_inflate_init(&zlib_ctx) < 0) {
+			printf("Failed to infalte init");
 			return EXIT_FAILURE;
 		}
-		ptr += 8;
-		ret=spdy_frame_parse(
-				&frm,
-				spdy_data_use(&data, pkg, ptr),
-				&zlib_ctx);
-		if(frm.type == SPDY_DATA_FRAME) {
-			printf("Dataframe: %*s",
-					((spdy_data_frame*)frm.frame)->length,
-					((spdy_data_frame*)frm.frame)->data);
+		while(ret == SPDY_ERROR_INSUFFICIENT_DATA) {
+			if(fread(pkg+ptr, 1, 8, f) != 8) {
+				printf("Can't read.\n");
+				return EXIT_FAILURE;
+			}
+			ptr += 8;
+			ret=spdy_frame_parse(
+					&frm,
+					spdy_data_use(&data, pkg, ptr),
+					&zlib_ctx);
+			if(ret == SPDY_ERROR_NONE) {
+				printf("LEN: %d\n", ptr);
+				if(frm.type == SPDY_DATA_FRAME) {
+					printf("Dataframe: %d %*s\n",
+							((spdy_data_frame*)frm.frame)->length,
+							((spdy_data_frame*)frm.frame)->length,
+							((spdy_data_frame*)frm.frame)->data);
+				} else if (frm.type == SPDY_CONTROL_FRAME) {
+					printf("Control frame.\n");
+				} else {
+					printf("Other frame.\n");
+				}
+			} else {
+				printf("Working...\n");
+			}
 		}
+		if(ret != SPDY_ERROR_NONE) {
+			printf("FAIL YO UDO\n");
+			return EXIT_FAILURE;
+		}
+		ptr=data.length;
+		ret=SPDY_ERROR_INSUFFICIENT_DATA;
 	}
 	printf("DONE\n");
 	return EXIT_SUCCESS;
