a6f4392cfcfd3ed25bf2a0e45aa1c2c8f87b420f
Thomas (nezza-_-) Roth <code@stacksmashing.net>
2011-06-17 17:09:15 +0200

    spdy_control_frame: Minor documentation improvements.

--- src/spdy_control_frame.h
+++ src/spdy_control_frame.h
@@ -12,23 +12,24 @@ extern const uint8_t SPDY_CONTROL_FRAME_MIN_LENGTH;
 
 /**
  * SPDY control frame types.
- * List of all SPDY control frame types.
+ * List of all SPDY control frame types. See section 2.7 in the draft 2
+ * specification.
  */
 enum SPDY_CTRL_TYPES {
-	SPDY_CTRL_SYN_STREAM=1,   /*!< SYN_STREAM */
-	SPDY_CTRL_SYN_REPLY=2,    /*!< SYN_REPLY */
-	SPDY_CTRL_RST_STREAM=3,   /*!< RST_STREAM */
-	SPDY_CTRL_SETTINGS=4,     /*!< SETTINGS */
-	SPDY_CTRL_NOOP=5,         /*!< NOOP */
-	SPDY_CTRL_PING=6,         /*!< PING */
-	SPDY_CTRL_GOAWAY=7,       /*!< GOAWAY */
-	SPDY_CTRL_HEADERS=8,      /*!< HEADERS */
-	SPDY_CTRL_WINDOW_UPDATE=9 /*!< WINDOW_UPDATE */
+	SPDY_CTRL_SYN_STREAM=1,   /*!< SYN_STREAM frame. */
+	SPDY_CTRL_SYN_REPLY=2,    /*!< SYN_REPLY frame. */
+	SPDY_CTRL_RST_STREAM=3,   /*!< RST_STREAM frame. */
+	SPDY_CTRL_SETTINGS=4,     /*!< SETTINGS frame. */
+	SPDY_CTRL_NOOP=5,         /*!< NOOP frame. */
+	SPDY_CTRL_PING=6,         /*!< PING frame.*/
+	SPDY_CTRL_GOAWAY=7,       /*!< GOAWAY frame. */
+	SPDY_CTRL_HEADERS=8,      /*!< HEADERS frame. */
+	SPDY_CTRL_WINDOW_UPDATE=9 /*!< WINDOW_UPDATE frame. */
 };
 
 /**
  * Control frame
- * Contains all data (including the data payload) of a data frame.
+ * - Contains all data (including the data payload) of a data frame.
  */
 typedef struct {
 	uint16_t version;   /*!< 15 bit version */
