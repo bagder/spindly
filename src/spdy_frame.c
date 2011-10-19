90a9a44bddf7a29df41609ff34c815e538534c2c
Thomas (nezza-_-) Roth <code@stacksmashing.net>
2011-10-19 02:18:43 +0200

    Remove dead code.

--- src/spdy_frame.c
+++ src/spdy_frame.c
@@ -10,11 +10,8 @@
  * Parse the header of a frame.
  * @param frame - Target frame.
  * @param data - Data to parse.
- * @param data_length - Length of data.
  * @see spdy_frame
- * @see spdy_control_frame
- * @see spdy_data_frame
- * @return 0 on success, -1 on failure.
+ * @return Errorcode
  */
 int spdy_frame_parse_header(
 		spdy_frame *frame,
@@ -28,43 +25,6 @@ int spdy_frame_parse_header(
 	 */
 	frame->type = (data->cursor[0] & 0x80)>>7;
 	frame->frame = NULL;
-#if 0
-	switch(frame->type) {
-		case SPDY_DATA_FRAME:
-			/** Allocate space for data frame. */
-			frame->frame = malloc(sizeof(spdy_data_frame));
-			if(!frame->frame) {
-				SPDYDEBUG("Allocating of space for data frame failed.");
-				return SPDY_ERROR_MALLOC_FAILED;
-			}
-			if((ret = spdy_data_frame_parse_header(
-							frame->frame,
-							data)) != SPDY_ERROR_NONE) {
-				free(frame->frame);
-				frame->frame = NULL;
-				return ret;
-			}
-			break;
-		case SPDY_CONTROL_FRAME:
-			/** Allocate space for control frame. */
-			frame->frame = malloc(sizeof(spdy_control_frame));
-			if(!frame->frame) {
-				SPDYDEBUG("Allocation of space for control frame failed.");
-				return SPDY_ERROR_MALLOC_FAILED;
-			}
-			spdy_control_frame_init(frame->frame);
-			/** Parse frame header. */
-			if((ret = spdy_control_frame_parse_header(frame->frame, data)) != SPDY_ERROR_NONE) {
-				free(frame->frame);
-				frame->frame = NULL;
-				return ret;
-			}
-			break;
-		default:
-			/** This should never happen. */
-			return SPDY_ERROR_INVALID_DATA;
-	}
-#endif
 	return SPDY_ERROR_NONE;
 }
 
