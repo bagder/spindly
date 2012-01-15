6f010c91233212593fac87720c06f0f426a37602
Cédric Deltheil <cedric@moodstocks.com>
2012-01-15 20:31:55 +0100

    bin/read_dump: fix compiler errors and simplify
    This tool now loads the input file in memory and delegates the whole
    parsing to the frame structure.

--- bin/read_dump.c
+++ bin/read_dump.c
@@ -11,82 +11,20 @@
 #include "spdy_zlib.h"
 #include "spdy_error.h"
 
-spdy_zlib_context ctx[2];
-
-int handle_syn_stream_frame(spdy_control_frame *frame, char *payload, FILE *f) {
-	(void)f;
-	spdy_syn_stream syn_stream;
-	spdy_data data;
-	int i;
-	if(spdy_syn_stream_parse(
-				&syn_stream,
-				spdy_data_use(&data, payload, frame->length),
-				frame->length,
-				&ctx[0]) != SPDY_ERROR_NONE) {
-		printf("Failed to parse SYN_STREAM.\n");
-		return EXIT_FAILURE;
-	}
-	printf("\tStream-ID:    % 5d\n", syn_stream.stream_id);
-	printf("\tAssociated:   % 5d\n", syn_stream.associated_to);
-	printf("\tPriority:     % 5d\n", syn_stream.priority);
-	printf("\tNV Block:     % 5d\n", syn_stream.nv_block->count);
-	for(i=0; i < syn_stream.nv_block->count; i++) {
-		printf("\t\t%s: %s\n", syn_stream.nv_block->pairs[i].name, syn_stream.nv_block->pairs[i].values);
-	}
-	//spdy_nv_block_destroy(syn_stream.nv_block);
-
-	return EXIT_SUCCESS;
-}
-
-int handle_syn_reply_frame(spdy_control_frame *frame, char *payload, FILE *f) {
-	(void)f;
-	spdy_syn_reply syn_reply;
-	spdy_data data;
-	int i;
-	if(spdy_syn_reply_parse(
-				&syn_reply,
-				spdy_data_use(&data, payload, frame->length),
-				frame->length,
-				&ctx[1]) != SPDY_ERROR_NONE) {
-		printf("Failed to parse SYN_REPLY.\n");
-		return EXIT_FAILURE;
-	}
-	printf("\tStream-ID:    % 5d\n", syn_reply.stream_id);
-	printf("\tNV Block:     % 5d\n", syn_reply.nv_block->count);
-	for(i=0; i < syn_reply.nv_block->count; i++) {
-		printf("\t\t%s: %s\n", syn_reply.nv_block->pairs[i].name, syn_reply.nv_block->pairs[i].values);
-	}
-	//spdy_nv_block_destroy(syn_reply.nv_block);
-
-	return EXIT_SUCCESS;
-}
-
-int handle_data_frame(spdy_frame *frame, FILE *f) {
-	spdy_data_frame *data_frm = (spdy_data_frame*)frame->frame.control;
-	char *payload;
+void handle_data_frame(spdy_frame *frame) {
+	spdy_data_frame *data_frm = (spdy_data_frame*)&frame->frame.data;
 	printf("Data frame:\n");
 	printf("\tStream ID:    % 5d\n", data_frm->stream_id);
 	printf("\tFlags:        % 5d\n", data_frm->flags);
 	printf("\tLength:       % 5d\n", data_frm->length);
-	payload = malloc(sizeof(char)*data_frm->length);
-	if(!payload) {
-		printf("Failed to allocate memory for payload.\n");
-		return EXIT_FAILURE;
-	}
-	if(fread(payload, 1, data_frm->length, f) != data_frm->length) {
-		printf("Failed to read payload from file.\n");
-		free(payload);
-		return EXIT_FAILURE;
-	}
-
-	free(payload);
-	return EXIT_SUCCESS;
 }
 
-int handle_control_frame(spdy_frame *frame, FILE *f) {
-	int ret = EXIT_SUCCESS;
+void handle_control_frame(spdy_frame *frame) {
+  int i;
+  spdy_syn_stream *syn_stream = NULL;
+  spdy_syn_reply* syn_reply = NULL;
 	spdy_control_frame *ctrl_frm =
-          (spdy_control_frame*)frame->frame.control;
+          (spdy_control_frame*)&frame->frame.control;
 	printf("Control frame:\n");
 	printf("\tVersion:      % 5d\n", ctrl_frm->version);
 	printf("\tType:         % 5d (%s)\n",
@@ -94,37 +32,50 @@ int handle_control_frame(spdy_frame *frame, FILE *f) {
 			spdy_control_frame_get_type_name(ctrl_frm->type));
 	printf("\tFlags:        % 5d\n", ctrl_frm->flags);
 	printf("\tLength:       % 5d\n", ctrl_frm->length);
-	char *payload = malloc(sizeof(char)*ctrl_frm->length);
-	if(!payload) {
-		printf("Failed to allocate memory for payload.\n");
-		return EXIT_FAILURE;
-	}
-	if(fread(payload, 1, ctrl_frm->length, f) != ctrl_frm->length) {
-		printf("Failed to read payload from file.\n");
-		ret = EXIT_FAILURE;
-	} else {
-		/* fread success */
-		switch(ctrl_frm->type) {
-			case SPDY_CTRL_SYN_STREAM:
-				// Skip the SYN_STREAM header to the NV block.
-				ret = handle_syn_stream_frame(ctrl_frm, payload, f);
-				break;
-			case SPDY_CTRL_SYN_REPLY:
-				ret = handle_syn_reply_frame(ctrl_frm, payload, f);
-				break;
-			default:
-				printf("Unknown frame type.");
-				ret = EXIT_SUCCESS;
-		}
-	}
-	free(payload);
-
-	return ret;
+	switch(ctrl_frm->type) {
+	  case SPDY_CTRL_SYN_STREAM:
+	    syn_stream = (spdy_syn_stream *)&ctrl_frm->obj.syn_stream;
+	    printf("\tStream-ID:    % 5d\n", syn_stream->stream_id);
+    	printf("\tAssociated:   % 5d\n", syn_stream->associated_to);
+    	printf("\tPriority:     % 5d\n", syn_stream->priority);
+    	printf("\tNV Block:     % 5d\n", syn_stream->nv_block.count);
+    	for(i=0; i < syn_stream->nv_block.count; i++) {
+    		printf("\t\t%s: %s\n", syn_stream->nv_block.pairs[i].name, syn_stream->nv_block.pairs[i].values);
+    	}
+      break;
+    case SPDY_CTRL_SYN_REPLY:
+      syn_reply = (spdy_syn_reply*)&ctrl_frm->obj.syn_reply;
+      printf("\tStream-ID:    % 5d\n", syn_reply->stream_id);
+      printf("\tNV Block:     % 5d\n", syn_reply->nv_block.count);
+      for(i=0; i < syn_reply->nv_block.count; i++) {
+        printf("\t\t%s: %s\n", syn_reply->nv_block.pairs[i].name, syn_reply->nv_block.pairs[i].values);
+      }
+      break;
+    default:
+      printf("Unknown frame type.");
+      break;
+  }
 }
 
-void end_zlib_contexts() {
-	spdy_zlib_inflate_end(&ctx[0]);
-	spdy_zlib_inflate_end(&ctx[1]);
+char *read_file(const char *filename, long *fsiz) {
+  FILE *f;
+  long siz;
+  char *buf;
+  size_t res;
+  f = fopen(filename, "rb");
+  if (f == NULL) return NULL;
+  fseek(f, 0, SEEK_END);
+  siz = ftell(f);
+  rewind(f);
+  buf = (char *) malloc(sizeof(char)*siz);
+  if (buf == NULL) return NULL;
+  res = fread(buf, 1, siz, f);
+  if (res != siz) {
+    free(buf);
+    return NULL;
+  }
+  *fsiz = siz;
+  return buf;
 }
 
 int main(int argc, char *argv[]) {
@@ -132,60 +83,32 @@ int main(int argc, char *argv[]) {
 		printf("Usage:\n%s [file]\n", argv[0]);
 		return EXIT_FAILURE;
 	}
-	FILE *f = fopen(argv[1], "r");
-	if(f == NULL) {
+  long fsiz;
+  char *fbuf = read_file(argv[1], &fsiz);
+	if(fbuf == NULL) {
 		printf("Failed to open file \"%s\".\n", argv[1]);
 		return EXIT_FAILURE;
 	}
-
-	if(spdy_zlib_inflate_init(&ctx[0]) != SPDY_ERROR_NONE) {
-		printf("Failed to inflate-init");
-		return EXIT_FAILURE;
-	}
-	if(spdy_zlib_inflate_init(&ctx[1]) != SPDY_ERROR_NONE) {
-		printf("Failed to inflate-init");
-		return EXIT_FAILURE;
-	}
-//	spdy_zlib_inflate_init(&ctx[1]);
-
-	char pkg[8];
-	while(1) {
-		if(fread(pkg, 1, 8, f) != 8) {
-			printf("EOF.\n");
-			end_zlib_contexts();
-			return EXIT_FAILURE;
-		}
-		spdy_frame frame;
-		if(spdy_frame_parse_header(&frame, (spdy_data *)&pkg[0]) !=
-                   SPDY_ERROR_NONE) {
-			free(frame.frame.control);
-			end_zlib_contexts();
-			printf("Failed to parse frame header.\n");
-			return EXIT_FAILURE;
-		}
-		switch(frame.type) {
-			case SPDY_DATA_FRAME:
-				if(handle_data_frame(&frame, f) == EXIT_FAILURE) {
-					free(frame.frame.control);
-					end_zlib_contexts();
-					return EXIT_FAILURE;
-				}
-				break;
-			case SPDY_CONTROL_FRAME:
-				if(handle_control_frame(&frame, f) == EXIT_FAILURE) {
-					free(frame.frame.control);
-					end_zlib_contexts();
-					return EXIT_FAILURE;
-				}
-				break;
-		}
-		free(frame.frame.control);
-		if(feof(f)) {
-			end_zlib_contexts();
-			return EXIT_SUCCESS;
-		}
-	}
-	fclose(f);
-	return EXIT_SUCCESS;
+	spdy_zlib_context zlib_ctx;
+	spdy_frame frame;
+	spdy_frame_init(&frame);
+	spdy_data data;
+	spdy_data_use(&data, fbuf, fsiz);
+	spdy_zlib_inflate_init(&zlib_ctx);
+  int ret = spdy_frame_parse(&frame, &data, &zlib_ctx);
+  if(ret == SPDY_ERROR_NONE) {
+    switch(frame.type) {
+      case SPDY_DATA_FRAME:
+        handle_data_frame(&frame);
+        break;
+      case SPDY_CONTROL_FRAME:
+        handle_control_frame(&frame);
+        break;
+    }
+  }
+  spdy_frame_destroy(&frame);
+  spdy_zlib_inflate_end(&zlib_ctx);
+  free(fbuf);
+  return EXIT_SUCCESS;
 }
 
